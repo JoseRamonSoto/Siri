d724ee8ebb523e42fe64cf9ebcf204a9dc578798
Jose Ramon Soto <student@meme-macbook.local>
2017-01-11 09:07:02 -0600

    Initial Commit

---
+++ Siri WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Siri WatchKit Extension
+//
+//  Created by Jose Ramon Soto on 1/10/17.
+//  Copyright © 2017 Jose Ramon Soto. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
